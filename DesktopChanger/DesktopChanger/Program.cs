b709ab467250f055334d7e3ea5874221c7b64d98
André Soares França <andre.franca@globalweb.com.br>
2019-07-22 10:36:51 -0400

    First Commit

---
+++ DesktopChanger/DesktopChanger/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DesktopChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\andre.franca\Pictures\Desktop";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",true);
+            var images = Directory.GetFiles(path);
+            if (images.Where(x => !x.Contains("Used-")).Count() == 0)
+            {
+                foreach(var image in images)
+                {
+                    Directory.Move(image, image.Replace("Used-", ""));
+                }
+                images = Directory.GetFiles(path);
+            }
+            var rand = new Random();
+            var imageChosen = images[rand.Next()%images.Count()];
+            Directory.Move(imageChosen, imageChosen.Insert(imageChosen.LastIndexOf("\\") + 1, "Used-"));
+            imageChosen = imageChosen.Insert(imageChosen.LastIndexOf("\\") + 1, "Used-");
+            key.SetValue("Wallpaper", imageChosen);
+        }
+    }
+}
